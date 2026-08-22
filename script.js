93e74ff5927433f740ca1f852f01c12f700e77eb
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-22 19:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const darkModeBtn = document.getElementById("darkModeBtn");
 darkModeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 });
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+// Login Function
+function login(){
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if(user.trim() == "admin" && pass.trim() == "1234"){
+        window.location.href = "dashboard.html";
+    }else{
+        document.getElementById("msg").innerHTML =
+        "Invalid Username or Password";
+    }
+}
