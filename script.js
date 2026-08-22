f9989343a3e78620f75b2cccfdbbd018bbbdc75c
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-22 18:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-document.querySelector("button").addEventListener("click", function() {
-    alert("Task Added Successfully!");
-});
-
-const darkBtn = document.getElementById("darkModeBtn");
-
-darkBtn.addEventListener("click", function(){
+document.getElementById("darkModeBtn").addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
 
-function updateClock(){
+function updateClock() {
     const now = new Date();
-    document.getElementById("clock").innerHTML =
-    now.toLocaleTimeString();
+    document.getElementById("clock").innerText =
+        now.toLocaleTimeString();
 }
 
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
 updateClock();
