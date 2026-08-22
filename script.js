6f122cdafad1baeafe690101dd572902345d95cb
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-22 18:31:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.querySelector("button").addEventListener("click", function() {
     alert("Task Added Successfully!");
 });
+
+const darkBtn = document.getElementById("darkModeBtn");
+
+darkBtn.addEventListener("click", function(){
+    document.body.classList.toggle("dark-mode");
+});
+
+function updateClock(){
+    const now = new Date();
+    document.getElementById("clock").innerHTML =
+    now.toLocaleTimeString();
+}
+
+setInterval(updateClock,1000);
+updateClock();
