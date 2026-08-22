bc1df6249bd1d9fd8a0ead1cfa84aa4707e43e1e
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-22 18:40:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 document.getElementById("darkModeBtn").addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 });
-
-function updateClock() {
-    const now = new Date();
-    document.getElementById("clock").innerText =
-        now.toLocaleTimeString();
-}
-
-setInterval(updateClock, 1000);
-updateClock();
