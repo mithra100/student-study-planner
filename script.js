c45aa0396d72902b79b72781464a534b90b91262
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-23 16:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 
-    function addTask() {
-     function completeTask(btn) {
+    function completeTask(btn) {
     let li = btn.parentElement;
     li.innerHTML = li.innerText.replace("Pending", "Completed");
 }
+
+function addTask() {
+
     let subject = document.getElementById("subject").value;
     let topic = document.getElementById("topic").value;
     let date = document.getElementById("date").value;
@@ -15,9 +17,10 @@
     }
 
     let li = document.createElement("li");
+
     li.innerHTML =
-subject + " - " + topic +
-" - Pending <button onclick='completeTask(this)'>✅ Complete</button>";
+    subject + " - " + topic +
+    " - Pending <button onclick='completeTask(this)'>✅ Complete</button>";
 
     document.getElementById("taskList").appendChild(li);
 
