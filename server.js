71ab801911316bb92154de1797d6c2109e32808f
mithra100 <saravanakumarmithra0@gmail.com>
2026-08-30 11:03:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.send("Student Study Planner Server is Running!");
 });
 
+app.get("/api/status", (req, res) => {
+  res.json({
+    success: true,
+    message: "Student Study Planner API is working"
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
