281193bd1ffd521bd561f3430a65e3a1bf808eec
mithra100 <saravanakumarmithra0@gmail.com>
2026-09-19 18:27:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -259,3 +259,38 @@ function updateTodayDate(){
     }
 
 }
+
+const quotes = [
+
+    "Success starts with self-discipline.",
+
+    "Small progress is still progress.",
+
+    "Study hard today, shine tomorrow.",
+
+    "Dream big and work hard.",
+
+    "Consistency beats motivation."
+
+];
+
+function loadQuote(){
+
+    const randomIndex =
+        Math.floor(
+            Math.random() * quotes.length
+        );
+
+    const quoteElement =
+        document.getElementById(
+            "dailyQuote"
+        );
+
+    if(quoteElement){
+
+        quoteElement.innerText =
+            quotes[randomIndex];
+
+    }
+
+}
