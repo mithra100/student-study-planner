ed0c4530257cda34bbc60f90f4d5a19679bc133b
mithra100 <saravanakumarmithra0@gmail.com>
2026-09-19 20:08:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -329,30 +329,20 @@ function checkQuiz() {
 
     let score = 0;
 
-    const q1 =
-    document.querySelector(
-    'input[name="q1"]:checked'
-    );
+    const q1 = document.querySelector('input[name="q1"]:checked');
+    const q2 = document.querySelector('input[name="q2"]:checked');
+    const q3 = document.querySelector('input[name="q3"]:checked');
+    const q4 = document.querySelector('input[name="q4"]:checked');
+    const q5 = document.querySelector('input[name="q5"]:checked');
 
-    const q2 =
-    document.querySelector(
-    'input[name="q2"]:checked'
-    );
+    if(q1 && q1.value === "a") score++;
+    if(q2 && q2.value === "a") score++;
+    if(q3 && q3.value === "a") score++;
+    if(q4 && q4.value === "a") score++;
+    if(q5 && q5.value === "a") score++;
 
-    if(q1 && q1.value === "a"){
-        score++;
-    }
-
-    if(q2 && q2.value === "a"){
-        score++;
-    }
-
-    document.getElementById(
-    "quizScore"
-    ).innerHTML =
-    "<h3>🎉 Your Score: "
-    + score +
-    "/2</h3>";
+    document.getElementById("quizScore").innerHTML =
+    "<h3>🎉 Your Score: " + score + "/5</h3>";
 }
 /* Exams */
 
